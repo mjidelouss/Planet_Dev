57eb5fc250672b0385816a46b63653a6722951b9
ABDELMAJID ELOUSSE <mjid.elouss@gmail.com>
2023-01-17 15:24:25 +0100

    admin auth

--- classes/article.class.php
+++ classes/article.class.php
@@ -1,6 +1,45 @@
 <?php
-require_once('../includes/autoloader.php');
+require_once('./includes/autoloader.php');
 
 class Article {
-  
+    public $id;
+    public $title;
+    public $author;
+    public $content;
+    public $category;
+    public $date;
+    public $db;
+
+    public function __construct() {
+        $this->db = new DbConnection;
+    }
+
+    public function display_articles() {
+        $sql="SELECT article.id, article.title, article.author, article.content, category.category AS category , article.published_date FROM  article, category WHERE category.id = article.category_id";
+        $stmt = $this->db->connect()->query($sql);
+        while ($row = $stmt->fetch()) 
+        {
+            $this->id = "".$row["id"] ."";
+            $this->title = "".$row["title"]."";
+            $this->author = "".$row["author"]."";
+            $this->content = "".$row["content"]."";
+            $this->category = "".$row["category"]."";
+            $this->date = "".$row["published_date"]."";
+            echo '
+                <tr>
+                <th scope="row">'.$this->id.'</th>
+                <td>'.$this->title.'</td>
+                <td>'.$this->author.'</td>
+                <td>'.$this->content.'</td>
+                <td>'.$this->category.'</td>
+                <td>'.$this->date.'</td>
+                <td><button data-info="'.$this->title.','.$this->author.','.$this->content.','.$this->category.','.$this->date.'" class="rounded" data-bs-toggle="modal" data-bs-target="#modal-updateArt" id="'.$this->id.'" onclick="initArt('.$this->id.')"><i class="bi bi-pencil-square" style="color: green;"></i></button></td>
+                <form action="" method="POST">
+                <input type="text" id="delid" name="delid" value="'.$this->id.'" style="display: none">
+                <td><button type="submit" class="rounded"><a href=""><i class="bi bi-trash-fill" style="color: red;"></i></a></button></td>
+                </form>
+                </tr>
+            ';
+        }
+    }
 }
--- classes/user.class.php
+++ classes/user.class.php
@@ -1,5 +1,5 @@
 <?php
-require_once('../includes/autoloader.php');
+require_once('./includes/autoloader.php');
 
 class User
 {
@@ -11,9 +11,47 @@ class User
     public function __construct() {
       $this->db = new DbConnection;
     }
+
+    public function regUser($email, $password)
+    {
+      //regular expression validation
+      if (!preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/", $email)) {
+        // $_SESSION['message'] = "Email Address is invalid!!";
+        header('location: index.php');
+        exit();
+      }
+      if (!preg_match("/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/", $password)) {
+        // $_SESSION['message'] = "Password must be at least 8 characters long contains 1 uppercase, 1 lowercase, 1 number and 1 special character!!";
+        header('location: index.php');
+        exit();
+      }
+      $sqlMail = "SELECT email FROM adminusers";
+      $stmt = $this->db->connect()->query($sqlMail);
+      while ($row = $stmt->fetch()) 
+      {
+        if ($email == $row["email"])
+        {
+            // $_SESSION['message'] = "Email Already Exists!!";
+            header('location: index.php');
+            exit();
+        }
+      }
+    }
+
     // function to log user to his intended dashboard
     public function login($email, $password)
     {
-        
+      $this->regUser($email, $password);
+      $sql = "SELECT id FROM adminusers WHERE email = '$email' AND password = '$password'";
+      $stmt = $this->db->connect()->query($sql);
+      $user = mysqli_fetch_assoc($stmt);
+      $_SESSION['user'] = $user['id'];
+      $count = mysqli_num_rows($stmt);
+      if ($count == 1) {
+          header("location: dashboard.php");
+      } else {
+          // $_SESSION['message'] = "Wrong Credentials!!";
+          header('location: index.php');
+      }
     }
 }
